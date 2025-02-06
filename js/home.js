8bdd1a45c08610d93634fd560e7a8f740f06bd3c
limjuhee0803 <limjuhee0803@naver.com>
2025-02-06 22:42:59 +0900

    코드 작성 및 수정

--- js/home.js
+++ js/home.js
@@ -128,7 +128,22 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
+//process 스와이퍼
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  slideToClickedSlide : true,
+  navigation:{
+      nextEl:".swiper-button-next",
+      prevEl:".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 20.
+    }
+}
+});
 
 
 
